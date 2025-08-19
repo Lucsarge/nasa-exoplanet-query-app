b485e59efeff31afed3a33f049268f5c57ca0394
Luc Sargent <lucsarge@gmail.com>
2025-08-19 10:21:59 -0800

    project: add default wpf app and window files

---
+++ nasa-exoplanet-query-app/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace nasa_exoplanet_query_app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
