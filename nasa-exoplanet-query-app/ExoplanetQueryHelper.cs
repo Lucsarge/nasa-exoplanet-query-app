22715049284d1d639abffdcc7bde027f37292100
Luc Sargent <lucsarge@gmail.com>
2025-08-25 13:46:08 -0800

    [EXOP-8] populate comboboxes with query data

---
+++ nasa-exoplanet-query-app/ExoplanetQueryHelper.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace nasa_exoplanet_query_app {
+    // Class for utilizing the Table Access Protocol (TAP) to query the Exoplanet Archive
+    // Documentation here: https://exoplanetarchive.ipac.caltech.edu/docs/TAP/usingTAP.html
+    public static class ExoplanetTAPHelper {
+        //public static readonly string EXOPLANET_ARCHIVE_BASE_URL = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?";
+        public static readonly string PlANETARY_SYSTEMS_TABLE = "ps";
+        public static readonly string SELECT_DISC_YEAR = "disc_year";
+        public static readonly string SELECT_DISC_METHOD = "discoverymethod";
+        public static readonly string SELECT_HOST_NAME = "hostname";
+        public static readonly string SELECT_DISC_FACILITY = "disc_facility";
+
+        public static readonly string EXOPLANET_ARCHIVE_BASE_URL = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=";
+
+        public static readonly string CSV_FORMAT = "&format=csv";
+
+        // selectParam must be separated by comma if multiple
+        // TODO: change distinct to use group by
+        public static string GetPSHTTPRequestString(string selectParam = "*", string format = "") {
+            string requestString = $"{EXOPLANET_ARCHIVE_BASE_URL}" +
+                                   $"select+distinct+{selectParam}" +
+                                   $"+from+{PlANETARY_SYSTEMS_TABLE}" +
+                                   $"+order+by+{selectParam}+asc" +
+                                   $"{format}";
+
+            return requestString;
+        }
+
+        // Builds a http request string that queries the Planetary System table for unique discovery years in ascending order
+        public static string GetPSDiscYearValuesString(string format = "") {
+            string requestString = EXOPLANET_ARCHIVE_BASE_URL +
+                                   "select+distinct+disc_year+from+ps+order+by+disc_year+asc";
+
+            if (!String.IsNullOrEmpty(format)) {
+                requestString += format;
+            }
+
+            return requestString;
+        }
+    }
+}
---
+++ nasa-exoplanet-query-app/MainWindowViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace nasa_exoplanet_query_app {
+    public class MainWindowViewModel : INotifyPropertyChanged {
+        private readonly string NOT_SPECIFIED = "N/S";
+
+        private int mDiscYearSelectedIndex;
+        public int DiscYearSelectedIndex {
+            get => mDiscYearSelectedIndex;
+            set {
+                mDiscYearSelectedIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string[] mDiscYearStrings;
+        public string[] DiscYearStrings {
+            get => mDiscYearStrings;
+            set {
+                mDiscYearStrings = CopyArrayValuesToDropDown(value);
+                OnPropertyChanged();
+            }
+        }
+
+        private int mDiscMethodSelectedIndex;
+        public int DiscMethodSelectedIndex {
+            get => mDiscMethodSelectedIndex;
+            set {
+                mDiscMethodSelectedIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string[] mDiscMethodStrings;
+        public string[] DiscMethodStrings {
+            get => mDiscMethodStrings;
+            set {
+                mDiscMethodStrings = CopyArrayValuesToDropDown(value);
+                OnPropertyChanged();
+            }
+        }
+
+        private int mHostNameSelectedIndex;
+        public int HostNameSelectedIndex {
+            get => mHostNameSelectedIndex;
+            set {
+                mHostNameSelectedIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string[] mHostNameStrings;
+        public string[] HostNameStrings {
+            get => mHostNameStrings;
+            set {
+                mHostNameStrings = CopyArrayValuesToDropDown(value);
+                OnPropertyChanged();
+            }
+        }
+
+        private int mDiscFacilitySelectedIndex;
+        public int DiscFacilitySelectedIndex {
+            get => mDiscFacilitySelectedIndex;
+            set {
+                mDiscFacilitySelectedIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string[] mDiscFacilityStrings;
+        public string[] DiscFacilityStrings {
+            get => mDiscFacilityStrings;
+            set {
+                mDiscFacilityStrings = CopyArrayValuesToDropDown(value);
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainWindowViewModel() {
+            mDiscYearStrings = ["N/S"];
+            mDiscYearSelectedIndex = 0;
+
+            mDiscMethodStrings = ["N/S"];
+            mDiscMethodSelectedIndex = 0;
+
+            mHostNameStrings = ["N/S"];
+            mHostNameSelectedIndex = 0;
+
+            mDiscFacilityStrings = ["N/S"];
+            mDiscFacilitySelectedIndex = 0;
+        }
+
+        private string[] CopyArrayValuesToDropDown(string[] values) {
+            string[] newStrings = new string[values.Length];
+            newStrings[0] = NOT_SPECIFIED; // Set the first value to Not Specifieds
+            // starting at 1 ignores the column header
+            for (int i = 1; i < values.Length; i++) {
+                newStrings[i] = values[i];
+            }
+
+            return newStrings;
+        }
+
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
