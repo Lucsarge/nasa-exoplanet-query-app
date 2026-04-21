223022f6e0b93212f622017c0edefe56d5b8da02
Luc Sargent <lucsarge@gmail.com>
2026-04-20 20:12:20 -0700

    [EXOP-16] added separate query and interactive view with navigation

---
+++ nasa-exoplanet-query-app/InteractiveViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasa_exoplanet_query_app {
+    internal class InteractiveViewModel {
+        public InteractiveViewModel() {
+
+        }
+    }
+}
